4f4935467e97330eb886e2799eb52fb8c68fee22
tsugumii21 <tsugumii21@gmail.com>
2025-05-18 21:36:34 +0800

    Update authController to replace 'fullName' with 'name' for consistency in user data handling

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -38,7 +38,7 @@ exports.login = async (req, res) => {
       token,
       user: {
         id: user._id,
-        fullName: user.fullName,
+        name: user.name,
         username: user.username,
         email: user.email,
         role: user.role,
